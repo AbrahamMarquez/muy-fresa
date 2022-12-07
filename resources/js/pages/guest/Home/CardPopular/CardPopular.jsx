d04ed70538aaa6f0494c79d99251a963d6b91085
marlingrAppConecte <marlingr.appconecte@gmail.com>
2022-12-07 17:43:51 -0600

    home half

---
+++ resources/js/pages/guest/Home/CardPopular/CardPopular.jsx
@@ -0,0 +1,55 @@
+//style
+import './CardPopular.scss'
+
+//assets
+import heartWhite from '../../../../img/icons/heartWhite.svg'
+import heartTransparent from '../../../../img/icons/heartTransparent.svg'
+
+export default function ({
+    img,
+    boxSize,
+    cuantity,
+    price,
+    selected,
+    id,
+    items,
+    setItems
+}) {
+
+    const handlePopular = () => {
+        const idxSelect = items.findIndex(element => element.id == id)
+
+        let newItems = [...items]
+        if(newItems[idxSelect].selected){
+            newItems[idxSelect].selected = false
+        }else{
+            newItems[idxSelect].selected = true
+        }
+
+        setItems(newItems)
+    }
+
+    return (
+        <div className="cardPopular">
+            <div className='img' style={{background: `url(${img})`}}></div>
+            <div className='data'>
+                <p className='box'>{boxSize}</p>
+                <hr />
+                <p className='cuantity'>{cuantity}</p>
+                <div className='priceContainer'>
+                    <p className='price'>{price}</p>
+                    {selected ?
+                        <button className='heartWhite' onClick={handlePopular}>
+                            <img src={heartWhite} />
+                        </button>
+                        :
+                        <button className='heartTransparent'>
+                            <img src={heartTransparent} />
+                        </button>
+                    }
+                </div>
+            </div>
+
+        </div>
+    )
+}
