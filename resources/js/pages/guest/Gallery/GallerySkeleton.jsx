8afddb8e4d4dcdefe7636edfcb14d547c670f1af
marlingr7 <marling.gu7@gmail.com>
2023-01-07 12:28:49 -0600

    skeleton

--- resources/js/pages/guest/Gallery/GallerySkeleton.jsx
+++ resources/js/pages/guest/Gallery/GallerySkeleton.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './Gallery.scss'
 
+//components
+import { DataView } from 'primereact/dataview'
+import { Skeleton } from 'primereact/skeleton'
+
 const GallerySkeleton = () => {
-  return (
-    <div>GallerySkeleton</div>
-  )
+
+    const data = [
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+    ]
+
+    const cardTemplate = (data) => {
+        return (
+            <div className='card'>
+                <div className='opacity'>
+                  <Skeleton width='100%' height='100%'/>
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <div className='gallery'>
+            <Skeleton width='20%' height='40px' borderRadius='10px'/>
+            <Skeleton width='30%' height='40px' borderRadius='10px'/>
+
+            <div className='picContainer'>
+                <div className="selectContainer">
+                    <Skeleton width='100%' height='100%' borderRadius='20px'/>
+                </div>
+
+                <div className='galleryDataView'>
+                    <DataView
+                        itemTemplate={cardTemplate}
+                        value={data}
+                        paginator
+                        rows={12}
+                        layout={"grid"}
+                    ></DataView>
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default GallerySkeleton
