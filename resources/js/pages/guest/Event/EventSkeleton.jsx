78cf333d1bc14c4a0e1236f5a015777eb141a0dc
marlingrAppConecte <marlingr.appconecte@gmail.com>
2023-01-09 11:11:53 -0600

    event rsponsive

--- resources/js/pages/guest/Event/EventSkeleton.jsx
+++ resources/js/pages/guest/Event/EventSkeleton.jsx
@@ -1,11 +1,206 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 //styles
 import './Event.scss'
 
+//components
+import { SectionEvent } from './SectionEvent/SectionEvent'
+import InputTime from '../../../components/atoms/InputTime/InputTime'
+import InputText from '../../../components/atoms/InputText/InputText'
+import InputNumber from '../../../components/atoms/InputNumber/InputNumber'
+import CheckboxCircle from '../../../components/atoms/CheckBoxCircle/CheckboxCircle'
+import { Carousel } from 'primereact/carousel';
+import TextArea from '../../../components/atoms/TextArea/TextArea'
+import { Skeleton } from 'primereact/skeleton'
+
 const EventSkeleton = () => {
+
+  const data = [
+    {
+      leftImg: false
+    },
+    {
+      leftImg: true
+    },
+    {
+      leftImg: false
+    },
+    {
+      leftImg: true
+    }
+  ]
+
+  const imgs = [1, 2, 3, 4, 5, 6, 7]
+
+  const responsiveOptions = [
+    {
+      breakpoint: '1024px',
+      numVisible: 3,
+      numScroll: 3
+    },
+    {
+      breakpoint: '968px',
+      numVisible: 1,
+      numScroll: 1
+    },
+  ];
+
+  const productTemplate = (product) => {
+    return (
+      <div className="product-item">
+        <Skeleton width='100%' height='100%' />
+      </div>
+    );
+  }
+
+  const [products, setProducts] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+
+  const [checkList, setCheckList] = useState([
+    1, 2, 3, 4, 5, 6
+  ])
+
   return (
-    <div>Event</div>
+    <div className='event'>
+      <div className='sections s1'>
+        <div className='s1-left'>
+          <div className='s1-left-images'></div>
+          <div className='hidden'>
+          <Skeleton width='50%' height='44px' />
+          </div>
+        </div>
+        <div className='s1-right'>
+          <Skeleton width='40%' height='100px' />
+          <Skeleton width='30%' height='80px' />
+          <Skeleton width='80%' height='40px' />
+          <div className='button'>
+            <Skeleton width='100%' height='100%' borderRadius='30px'/>
+          </div>
+          <div className='button'>
+            <Skeleton width='100%' height='100%' borderRadius='30px' />
+          </div>
+
+          <Skeleton width='40%' height='44px' />
+        </div>
+      </div>
+
+      <div>
+        {
+          data.map((item, idx) => (
+            <SectionEvent skeleton data={item} key={idx} />
+          ))
+        }
+      </div>
+
+      <div className='sections s2'>
+        <div className='s2-left'>
+          <div className='imgContainer'>
+
+          </div>
+          <div className='name'>
+            <Skeleton width='100px' height='30px' />
+            <div>
+              <Skeleton width='100px' height='22px' />
+              <ul>
+                <Skeleton width='100px' height='22px' />
+                <Skeleton width='100px' height='22px' />
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className='s2-right'>
+          <div className='container'>
+            <Skeleton width='100px' height='22px' />
+            <div className='containerPrices'>
+              <Skeleton width='50px' height='50px' shape='circle' />
+              <div>
+                <Skeleton width='100px' height='22px' />
+                <Skeleton width='100px' height='22px' />
+              </div>
+            </div>
+            <div className='containerPrices'>
+              <Skeleton width='50px' height='50px' shape='circle' />
+              <div>
+                <Skeleton width='100px' height='22px' />
+                <Skeleton width='100px' height='22px' />
+              </div>
+            </div>
+            <div className='containerPrices'>
+              <Skeleton width='50px' height='50px' shape='circle' />
+              <div>
+                <Skeleton width='100px' height='22px' />
+                <Skeleton width='100px' height='22px' />
+              </div>
+            </div>
+
+            <Skeleton width='100px' height='22px' />
+            <Skeleton width='100px' height='22px' />
+            <Skeleton width='100px' height='22px' />
+
+          </div>
+        </div>
+
+      </div>
+
+      <div className='sections s3'>
+        <div className='s3-left'>
+          <Skeleton width='100px' height='22px' />
+          <Skeleton width='100px' height='22px' />
+
+          <div className='imgContainerAll'>
+
+            {
+              imgs.map((item, idx) => (
+                <div key={idx} className='imgContainer'>
+                  <Skeleton width='100px' height='100px' />
+                </div>
+              ))
+            }
+
+          </div>
+
+        </div>
+        <div className='s3-right'>
+          <Skeleton width='100px' height='22px' />
+          <Skeleton width='100px' height='22px' />
+
+          <form className='form'>
+            <div style={{ display: 'flex', alignItems: 'flex-end' }}>
+              <Skeleton width='50%' height='45px' />
+
+              <InputTime className={'ml-10 w-30'} skeleton />
+            </div>
+            <InputText className={'mt-20'} skeleton />
+            <div className='d-flex mt-20'>
+              <InputText className={'w-40'} skeleton />
+              <InputNumber className={'w-40 ml-10'} skeleton />
+            </div>
+            <InputText className={'mt-20'} skeletons />
+            <div className='d-flex mt-20'>
+              <InputNumber skeleton />
+              <div className='ml-10'>
+                <Skeleton width='100px' height='22px' />
+                {
+                  checkList.map((item, idx) => (
+                    <CheckboxCircle key={idx} className={'check'} skeleton></CheckboxCircle>
+                  ))
+                }
+              </div>
+            </div>
+            <TextArea className={'mt-20'} skeleton />
+            <Skeleton width='100%' height='45%' borderRadius='20px' />
+          </form>
+
+
+        </div>
+
+      </div >
+      <div className='s4'>
+        <Carousel value={products} numVisible={3} numScroll={3} responsiveOptions={responsiveOptions}
+          itemTemplate={productTemplate} />
+      </div>
+
+    </div >
   )
 }
 
--- resources/js/pages/guest/Event/SectionEvent/SectionEvent.jsx
+++ resources/js/pages/guest/Event/SectionEvent/SectionEvent.jsx
@@ -6,60 +6,95 @@ import './SectionEvent.scss'
 
 //assets
 import whatsapp from '../../../../img/icons/whatsapp.svg'
+import { Skeleton } from 'primereact/skeleton'
 
-export const SectionEvent = ({ data }) => {
+export const SectionEvent = ({ data, skeleton }) => {
     return (
-        <div className='section'>
+        <>
             {
-                data.leftImg &&
-                <div className='imgLeft'>
-                    <div className='imgContainer'>
-                        <img src={data.img} />
-                    </div>
+                !skeleton ?
 
-                </div>
-            }
-            <div className='info'>
-                <h3 className='title'>{data.title}</h3>
-                {
-                    data.subtitles.map((item, idx) => (
-                        <div key={idx} className='subtitle'>
-                            <div>
-                                <h5>{item.name}</h5>
-                                <span>({item.quantity})</span>
+                    <div className='section'>
+                        {
+                            data.leftImg &&
+                            <div className='imgLeft'>
+                                <div className='imgContainer'>
+                                    <img src={data.img} />
+                                </div>
+
                             </div>
-                            <h3>{item.price} <span>MXN</span></h3>
+                        }
+                        <div className='info'>
+                            <h3 className='title'>{data.title}</h3>
+                            {
+                                data.subtitles.map((item, idx) => (
+                                    <div key={idx} className='subtitle'>
+                                        <div>
+                                            <h5>{item.name}</h5>
+                                            <span>({item.quantity})</span>
+                                        </div>
+                                        <h3>{item.price} <span>MXN</span></h3>
+                                    </div>
+                                ))
+                            }
+
+                            <p className='includes'>Incluye:</p>
+                            <ul>
+                                {
+                                    data.includes.map((item, idx) => (
+                                        <li key={idx}>{item}</li>
+                                    ))
+                                }
+                            </ul>
+
+                            {
+                                data.information && <p className='information'>{data.information}</p>
+                            }
+
+                            <button>
+                                <img src={whatsapp} />
+                                Personalizarla
+                            </button>
                         </div>
-                    ))
-                }
+                        {
+                            !data.leftImg &&
+                            <div className='imgRight'>
+                                <div className='imgContainer'>
+                                    <img src={data.img} />
+                                </div>
+                            </div>
+                        }
 
-                <p className='includes'>Incluye:</p>
-                <ul>
-                    {
-                        data.includes.map((item, idx) => (
-                            <li key={idx}>{item}</li>
-                        ))
-                    }
-                </ul>
-
-                {
-                    data.information && <p className='information'>{data.information}</p>
-                }
-
-                <button>
-                    <img src={whatsapp}/>
-                    Personalizarla
-                </button>
-            </div>
-            {
-                !data.leftImg &&
-                <div className='imgRight'>
-                    <div className='imgContainer'>
-                        <img src={data.img} />
                     </div>
-                </div>
-            }
+                    :
+                    <div className='section'>
+                        {
+                            data.leftImg &&
+                            <div className='imgLeft'>
+                                <div className='imgContainer'>
+                                    <Skeleton width='100%' height='100%' shape='circle' />
+                                </div>
 
-        </div>
+                            </div>
+                        }
+                        <div className='info'>
+                            <Skeleton width='100px' height='40px' />
+
+
+                            <Skeleton width='100px' height='40px' borderRadius='20px' />
+
+                        </div>
+                        {
+                            !data.leftImg &&
+                            <div className='imgRight'>
+                                <div className='imgContainer'>
+                                    <Skeleton width='100%' height='100%' />
+                                </div>
+                            </div>
+                        }
+
+                    </div>
+            }
+        </>
     )
 }
